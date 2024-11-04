0ddfac042693ba1da9af76ea1c08882f269c88d5
GayendriLalithya <rajanayakegayendri@gmail.com>
2024-11-04 09:13:08 +0530

    Back to initial application

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/register', function () {
-    return view('register');
-});
-
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
-
-Route::get('/complete_profile/{user_id}', [UserProfileController::class, 'edit'])->name('complete_profile');
-Route::get('/dashboard/admin', [AdminController::class, 'index'])->name('admin.dashboard');
-Route::get('/dashboard/customer', [CustomerController::class, 'index'])->name('customer.dashboard');
-
-
